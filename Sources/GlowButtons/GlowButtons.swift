2faa54edeb65d85954bf5e2600385bee1c00123e
steph45607 <staniswinata.stephanie@gmail.com>
2025-12-18 19:14:41 +0700

    fixed GButton

--- Sources/GlowButtons/GlowButtons.swift
+++ Sources/GlowButtons/GlowButtons.swift
@@ -2,6 +2,24 @@
 // https://docs.swift.org/swift-book
 import SwiftUI
 
+public struct GButton: View{
+    public var body: some View{
+        VStack{
+            HStack{
+                Image(systemName: "heart")
+                Text("Hearts")
+            }
+        }
+        .padding()
+        .cornerRadius(50)
+        .overlay(
+            RoundedRectangle(cornerRadius: 50)
+                .stroke(.red)
+                .shadow(color: .red, radius: 2)
+        )
+    }
+}
+
 struct GlowButton: View {
     var body: some View {
         VStack{
@@ -22,7 +40,5 @@ struct GlowButton: View {
 
 #Preview {
     GlowButton()
-    GlowButton()
-    GlowButton()
-    GlowButton()
+    GButton()
 }
