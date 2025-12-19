157f14668b9cd1332ee03e00d414b09de1ff4ff8
steph45607 <staniswinata.stephanie@gmail.com>
2025-12-19 12:25:58 +0700

    fixed init

--- Sources/GlowButtons/GlowButtons.swift
+++ Sources/GlowButtons/GlowButtons.swift
@@ -2,7 +2,31 @@
 // https://docs.swift.org/swift-book
 import SwiftUI
 
-internal struct GButton: View{
+public struct GlowButtonConfiguration{
+    public let text: String
+    public let image: String
+    
+    public let showGlow: Bool
+    public let color: Color
+    
+    public let cornerRadius: CGFloat
+    
+//    internal init(text: String, image: String, showGlow: Bool, color: Color, cornerRadius: CGFloat) {
+//        self.text = text
+//        self.image = image
+//        self.showGlow = showGlow
+//        self.color = color
+//        self.cornerRadius = cornerRadius
+//    }
+    
+    public init(text: String = "Heart", image: String = "Heart", showGlow: Bool = true, color: Color = .red, cornerRadius: CGFloat = 10) {
+        self.init(
+            text: text, image: image, showGlow: showGlow, color: color, cornerRadius: cornerRadius
+        )
+    }
+}
+
+public struct GButton: View{
     public var body: some View{
         VStack{
             HStack{
@@ -20,25 +44,8 @@ internal struct GButton: View{
     }
 }
 
-struct GlowButton: View {
-    var body: some View {
-        VStack{
-            HStack{
-                Image(systemName: "heart")
-                Text("Heart")
-            }
-        }
-        .padding()
-        .cornerRadius(50)
-        .overlay(
-            RoundedRectangle(cornerRadius: 50)
-                .stroke(.red)
-                .shadow(color: .red, radius: 2)
-        )
-    }
-}
 
 #Preview {
-    GlowButton()
+//    GlowButton()
     GButton()
 }
